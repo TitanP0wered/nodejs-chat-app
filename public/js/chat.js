1c25945e2381b375f62f5d95831c1367ca79b25b
TitanP0wered <76790421+TitanP0wered@users.noreply.github.com>
2021-07-14 11:49:36 +0100

    Update chat.js

--- public/js/chat.js
+++ public/js/chat.js
@@ -4,16 +4,12 @@ const socket = io();
 const $messageForm = document.querySelector("#message-form");
 const $messageFormInput = $messageForm.querySelector("input");
 const $messageFormButton = $messageForm.querySelector("button");
-const $sendLocationBtn = document.querySelector("#send-location");
 const $messages = document.querySelector("#messages");
 
 // Templates
 const messageTemplate = document.querySelector("#message-template").innerHTML;
-const locationMessageTemplate = document.querySelector("#location-message-template").innerHTML;
 const sidebarTemplate = document.querySelector("#sidebar-template").innerHTML;
 
-// Options
-const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true });
 
 const autoscroll = () => {
   // New message element
@@ -49,16 +45,6 @@ socket.on("message", message => {
   autoscroll();
 });
 
-socket.on("locationMessage", message => {
-  console.log(message);
-  const html = Mustache.render(locationMessageTemplate, {
-    username: message.username,
-    url: message.url,
-    createdAt: moment(message.createdAt).format("h:mm a")
-  });
-
-  $messages.insertAdjacentHTML("beforeend", html);
-});
 
 socket.on("roomData", ({ room, users }) => {
   const html = Mustache.render(sidebarTemplate, {
@@ -87,30 +73,6 @@ $messageForm.addEventListener("submit", e => {
       console.log("Message delivered!");
     }
   });
-});
-
-$sendLocationBtn.addEventListener("click", () => {
-  if (!navigator.geolocation) {
-    return alert("Geolocation is not supported by your browser.");
-  } else {
-    $sendLocationBtn.setAttribute("disabled", "disabled");
-
-    navigator.geolocation.getCurrentPosition(position => {
-      socket.emit(
-        "sendLocation",
-        {
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude
-        },
-        error => {
-          $sendLocationBtn.removeAttribute("disabled");
-          if (!error) {
-            console.log("Location shared!");
-          }
-        }
-      );
-    });
-  }
 });
 
 socket.emit("join", { username, room }, error => {
