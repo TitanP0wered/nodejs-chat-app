bf26592c26ce1d412f75cb10208e86bc85c6c061
TitanP0wered <76790421+TitanP0wered@users.noreply.github.com>
2021-07-14 11:13:56 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ const path = require("path");
 const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
-const Filter = require("bad-words");
 const { generateMessage, generateLocationMessage } = require("./utils/messages");
 const { addUser, removeUser, getUser, getUsersInRoom } = require("./utils/users");
 
@@ -28,7 +27,7 @@ io.on("connection", socket => {
       socket.join(user.room);
 
       socket.emit("message", generateMessage("Admin", "Welcome!"));
-      socket.broadcast.to(user.room).emit("message", generateMessage("Admin", `${user.username} has joined!`));
+      socket.broadcast.to(user.room).emit("message", generateMessage("Admin", `${user.username} has joined the "fun"!`));
       io.to(user.room).emit("roomData", {
         room: user.room,
         users: getUsersInRoom(user.room)
@@ -42,25 +41,17 @@ io.on("connection", socket => {
     const user = getUser(socket.id);
     const filter = new Filter();
 
-    if (filter.isProfane(message)) {
-      return callback("Profanity is not allowed!");
-    } else {
+    {
       io.to(user.room).emit("message", generateMessage(user.username, message));
       callback();
     }
-  });
-
-  socket.on("sendLocation", (coords, callback) => {
-    const user = getUser(socket.id);
-    io.to(user.room).emit("locationMessage", generateLocationMessage(user.username, `https://www.google.com/maps?q=${coords.latitude},${coords.longitude}`));
-    callback();
   });
 
   socket.on("disconnect", () => {
     const user = removeUser(socket.id);
 
     if (user) {
-      io.to(user.room).emit("message", generateMessage("Admin", `${user.username} has left!`));
+      io.to(user.room).emit("message", generateMessage("Admin", `${user.username} has left! haha L noob.`));
       io.to(user.room).emit("roomData", {
         room: user.room,
         users: getUsersInRoom(user.room)
